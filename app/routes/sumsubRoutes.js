1eb37c4fc14ccc011cdac095be9f1ae26edd1a6b
hyazharmehmood <hyazharmehmood@gmail.com>
2025-08-19 18:40:27 +0500

    fixes

--- app/routes/sumsubRoutes.js
+++ app/routes/sumsubRoutes.js
@@ -5,7 +5,8 @@ import { createApplicantHandler, uploadDocumentHandler, webhookHandler, uploadDo
 const router = express.Router();
 
 router.post('/create-applicant', createApplicantHandler);
-router.get('/webhook', webhookHandler);
+router.post('/webhook', webhookHandler);
+router.post('/webhooks', webhookHandler);
 router.post('/upload-document', uploadDocumentHandler);
 router.post('/upload-document-data', uploadDocumentDataOnlyHandler);
 
